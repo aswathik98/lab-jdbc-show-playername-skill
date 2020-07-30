7f9edf3a0096cb2e92da30f71dbbcbbe80767444
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-30 22:34:05 +0530

    Add files via upload

--- src/model/Skill.java
+++ src/model/Skill.java
@@ -1,1 +1,33 @@
+package model;
 
+public class Skill{
+	
+	private Long skillld ;
+	
+	private String skillName ;
+
+	public Long getSkillld() {
+		return skillld;
+	}
+
+	public void setSkillld(Long skillld) {
+		this.skillld = skillld;
+	}
+
+	public String getSkillName() {
+		return skillName;
+	}
+
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+
+	public Skill(Long skillld, String skillName) {
+	
+		this.skillld = skillld;
+		this.skillName = skillName;
+	}
+	
+	
+	
+}
